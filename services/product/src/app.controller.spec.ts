a30cfd137b8cc9131109fd2bbfad4aad987c6849
cercinc <caglar.cercinli@vrt.be>
2023-01-29 21:10:30 +0100

    after merge2

--- services/product/src/app.controller.spec.ts
+++ services/product/src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return samsung', () => {
       expect(productController.FindOne({ id: 1 })).toStrictEqual({
         id: 1,
         name: 'samsung',
